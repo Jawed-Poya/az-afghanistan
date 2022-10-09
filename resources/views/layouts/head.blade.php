be5ff4883e73d289040c1c4dc1efefe63ac35c84
Jawed-Poya <jawedpoya.001@gmail.com>
2022-10-09 11:48:36 +0430

    Changes to the head and foot linking

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -9,49 +9,24 @@
     <meta content="@yield('keywords', '')" name="keywords">
 
     <!-- Fav icons -->
-    <link href="{{ asset('img/favicon.png') }}" rel="icon">
-    <link href="{{ asset('img/apple-touch-icon.png') }} " rel="apple-touch-icon">
-
-    <!-- Google Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+    <link href="{{ secure_asset('img/logo.png') }}" rel="icon">
+    <link href="{{ secure_asset('img/logo.png') }} " rel="apple-touch-icon">
 
     <!-- Vendor CSS Files -->
-    <link href="{{ asset('vendor/animate.css/animate.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('vendor/aos/aos.css') }}" rel="stylesheet">
-    <link href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
-    <link href="{{ asset('vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('vendor/remixicon/remixicon.css') }}" rel="stylesheet">
-    <link href="{{ asset('vendor/swiper/swiper-bundle.min.css') }}'" rel="stylesheet">
+    <link href="{{ secure_asset('vendor/animate.css/animate.min.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('vendor/aos/aos.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('vendor/remixicon/remixicon.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('vendor/swiper/swiper-bundle.min.css') }}'" rel="stylesheet">
 
     <!-- Template Main CSS File -->
-    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/style.css') }}" rel="stylesheet">
 
     @if(\App\Helpers\Tools::instance()->getLanguage() != "en")
-    <link href="{{ asset('css/style-rtl.css') }}" rel="stylesheet">
+        <link href="{{ secure_asset('css/style-rtl.css') }}" rel="stylesheet">
     @endif
-
-    @production
-        <!-- Fav icons -->
-        <link href="{{ secure_asset('img/favicon.png') }}" rel="icon">
-        <link href="{{ secure_asset('img/apple-touch-icon.png') }} " rel="apple-touch-icon">
-
-        <!-- Vendor CSS Files -->
-        <link href="{{ secure_asset('vendor/animate.css/animate.min.css') }}" rel="stylesheet">
-        <link href="{{ secure_asset('vendor/aos/aos.css') }}" rel="stylesheet">
-        <link href="{{ secure_asset('vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
-        <link href="{{ secure_asset('vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
-        <link href="{{ secure_asset('vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
-        <link href="{{ secure_asset('vendor/remixicon/remixicon.css') }}" rel="stylesheet">
-        <link href="{{ secure_asset('vendor/swiper/swiper-bundle.min.css') }}'" rel="stylesheet">
-
-        <!-- Template Main CSS File -->
-        <link href="{{ secure_asset('css/style.css') }}" rel="stylesheet">
-
-        @if(\App\Helpers\Tools::instance()->getLanguage() != "en")
-            <link href="{{ secure_asset('css/style-rtl.css') }}" rel="stylesheet">
-        @endif
-    @endproduction
 
     @livewireStyles
 
