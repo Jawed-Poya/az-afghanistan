44befb38432d86646fc999b2f1cabaf6353dcf2c
Jawed-Poya <jawedpoya.001@gmail.com>
2022-10-09 11:19:11 +0430

    Change asset to secure_asset for production purpose

--- resources/views/layouts/foot.blade.php
+++ resources/views/layouts/foot.blade.php
@@ -77,15 +77,15 @@
     <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
     <!-- Vendor JS Files -->
-    <script src="{{ asset('vendor/purecounter/purecounter_vanilla.js') }}"></script>
-    <script src="{{ asset('vendor/aos/aos.js') }}"></script>
-    <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ asset('vendor/swiper/swiper-bundle.min.js') }}"></script>
-    <script src="{{ asset('vendor/php-email-form/validate.js') }}"></script>
+    <script src="{{ secure_asset('vendor/purecounter/purecounter_vanilla.js') }}"></script>
+    <script src="{{ secure_asset('vendor/aos/aos.js') }}"></script>
+    <script src="{{ secure_asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ secure_asset('vendor/swiper/swiper-bundle.min.js') }}"></script>
+    <script src="{{ secure_asset('vendor/php-email-form/validate.js') }}"></script>
 
     <!-- Template Main JS File -->
-    <script src="{{ asset('js/main.js') }}"></script>
-    <script src="{{ asset('js/script.js') }}"></script>
+    <script src="{{ secure_asset('js/main.js') }}"></script>
+    <script src="{{ secure_asset('js/script.js') }}"></script>
     @livewireScripts
 
     @stack('scripts')
